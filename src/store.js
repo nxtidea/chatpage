5e8953a8bcf4e0d4754c9276214b36dba4d5e179
nxtidea <lnetact@outlook.com>
2023-11-01 12:05:43 +0800

    feat: browser storage

--- src/store.js
+++ src/store.js
@@ -1,11 +1,18 @@
-import { reactive, computed, ref } from 'vue'
+import { reactive, computed, ref, watch } from 'vue'
 
-export const journalSessions = reactive([
-    // { name: '南京', focused: true, question: "", wrapper: [], loading: false, info: {} },
-])
+export const journalSessions = reactive(JSON.parse(localStorage.getItem('journalSessions')) || [])
 
 export const currentSession = computed(() => {
     return journalSessions.find(session => session.focused === true)
 })
 
-export const showInfoForm = ref(false)
+export const showInfoForm = ref(JSON.parse(localStorage.getItem('showInfoForm')) || false)
+
+watch(journalSessions, (newValue) => {
+    localStorage.setItem('journalSessions', JSON.stringify(newValue))
+})
+
+watch(showInfoForm, (newValue) => {
+    localStorage.setItem('showInfoForm', JSON.stringify(newValue))
+})
+
